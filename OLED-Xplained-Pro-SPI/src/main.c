02f4b3c3b94989b00b7eb89fb529b16258c28b72
joaogcfa <joaogcfa@al.insper.edu.br>
2021-04-19 14:29:17 -0300

    defines de led e botao e init de led e botoes

--- OLED-Xplained-Pro-SPI/src/main.c
+++ OLED-Xplained-Pro-SPI/src/main.c
@@ -1,23 +1,69 @@
-#include <asf.h>
+#include <asf.h> 
 
 #include "gfx_mono_ug_2832hsweg04.h"
 #include "gfx_mono_text.h"
 #include "sysfont.h"
 
-int main (void)
-{
-	board_init();
+//LED1
+#define LED1_PIO      PIOA
+#define LED1_PIO_ID   ID_PIOA
+#define LED1_IDX      0
+#define LED1_IDX_MASK (1 << LED1_IDX)
+
+//LED3
+#define LED3_PIO      PIOB
+#define LED3_PIO_ID   ID_PIOB
+#define LED3_IDX      2
+#define LED3_IDX_MASK (1 << LED3_IDX)
+
+// Botao 1
+#define BUT1_PIO      PIOD
+#define BUT1_PIO_ID   ID_PIOD
+#define BUT1_IDX  28
+#define BUT1_IDX_MASK (1 << BUT1_IDX)
+
+// Botao 2
+#define BUT2_PIO      PIOC
+#define BUT2_PIO_ID   ID_PIOC
+#define BUT2_IDX  31
+#define BUT2_IDX_MASK (1 << BUT2_IDX)
+
+//Botao 3
+# define BUT3_PIO      PIOA
+# define BUT3_PIO_ID   ID_PIOA
+# define BUT3_IDX  19
+# define BUT3_IDX_MASK (1 << BUT3_IDX)
+
+void init(void){
+	// Initialize the board clock
 	sysclk_init();
+	board_init();
 	delay_init();
 
-  // Init OLED
-	gfx_mono_ssd1306_init();
-  
-  // Escreve na tela um circulo e um texto
-	gfx_mono_draw_filled_circle(20, 16, 16, GFX_PIXEL_SET, GFX_WHOLE);
-  gfx_mono_draw_string("mundo", 50,16, &sysfont);
+	// Desativa WatchDog Timer
+	WDT->WDT_MR = WDT_MR_WDDIS;
+	
+	
+	//Inicializa o led
+	pmc_enable_periph_clk(LED1_PIO_ID);
+	pio_configure(LED1_PIO, PIO_OUTPUT_1, LED1_IDX_MASK, PIO_DEFAULT);
+	
+	pmc_enable_periph_clk(LED3_PIO_ID);
+	pio_configure(LED3_PIO, PIO_OUTPUT_1, LED3_IDX_MASK, PIO_DEFAULT);
+}
 
-  /* Insert application code here, after the board has been initialized. */
+int main (void)
+{
+	init();
+
+	// Init OLED
+	gfx_mono_ssd1306_init();
+	
+	// Escreve na tela um circulo e um texto
+	gfx_mono_draw_filled_circle(20, 16, 16, GFX_PIXEL_SET, GFX_WHOLE);
+	gfx_mono_draw_string("mundo", 50,16, &sysfont);
+
+	/* Insert application code here, after the board has been initialized. */
 	while(1) {
 
 	}
